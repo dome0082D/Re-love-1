570f5c6b6664bb8e4077d35690ae0d97da407c85
dome0082D <dome0082@gmail.com>
2026-04-21 11:55:47 +0200

    vercel

--- app/api/stripe/sponsor/route.ts
+++ app/api/stripe/sponsor/route.ts
@@ -3,7 +3,7 @@ import Stripe from 'stripe';
 
 // Inizializza Stripe con la tua chiave segreta
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: '2023-10-16',
+  apiVersion: '2026-03-25',
 });
 
 export async function POST(req: Request) {
